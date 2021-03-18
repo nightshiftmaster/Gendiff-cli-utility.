376ffe6858b8a990eb66fbb431958d462c596c1a
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2021-03-18 21:11:13 +0200

    bin update

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,8 @@ const { program } = require('commander')
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 
   program.parse(process.argv)
