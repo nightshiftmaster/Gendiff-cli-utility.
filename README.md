f3665aa2925825e68fe7ac4f3bc4e8f5017cca4c
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2021-03-26 21:49:43 +0300

    add Index.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/nightshiftmaster/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/nightshiftmaster/frontend-project-lvl2/actions)
+
+<a href="https://asciinema.org/a/fn6q5hpSNShldXhm7BT3vKpSS" target="_blank"><img src="https://asciinema.org/a/fn6q5hpSNShldXhm7BT3vKpSS.svg" /></a>
--- gendiff.js
+++ gendiff.js
@@ -1,41 +1,13 @@
 #!/usr/bin/env node
 
-const _ = require('lodash');
-
-const path = require('path');
-
-const { readFileSync } = require('fs');
-
-const { program } = require('commander');
+import program from 'commander';
+import gendiff from './lib/index.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => {
-    filepath1 = readFileSync('/Users/nightshiftmaster/Desktop/frontend-project-lvl2/src/file1.json', 'utf8');
-    filepath2 = readFileSync('/Users/nightshiftmaster/Desktop/frontend-project-lvl2/src/file2.json', 'utf8');
-    const json1 = JSON.parse(filepath1);
-    const json2 = JSON.parse(filepath2);
-    const keys = _.sortBy(_.union(_.keys(json1), _.keys(json2)));
-    const mergedJson = _.merge(_.clone(json1), _.clone(json2));
-    const compareJson = keys.reduce((newJson, key) => {
-      let statusSymbol = '';
-      if (!_.has(json2, key)) {
-        statusSymbol = '-';
-      } else if (!_.has(json1, key)) {
-        statusSymbol = '+';
-      } else if (mergedJson[key] !== json1[key]) {
-        statusSymbol = '+';
-        newJson = [...newJson, ` ${'-'} ${key}: ${json1[key]}`];
-      } else {
-        statusSymbol = ' ';
-      }
-      const line = [...newJson, ` ${statusSymbol} ${key}: ${mergedJson[key]}`];
-      return line;
-    }, []);
-    const comparedJson = ['{', ...compareJson, '}'].join('\n');
-    console.log(comparedJson);
-  });
+  .action((filepath1, filepath2) => gendiff(filepath1, filepath2));
+
 program.parse(process.argv);
