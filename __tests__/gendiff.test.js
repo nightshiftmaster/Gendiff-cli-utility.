40fe5fdde68736c943da7c48551dfa1c829faa19
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2021-03-28 21:20:27 +0300

    test update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,42 @@
 import {
   describe, it, expect,
 } from '@jest/globals';
-import gendiff from '../lib/index';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import gendiff from '../lib/index.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const makeDiff = gendiff;
 
-describe('genDiff', () => {
-  it('test 1', () => {
-    const expexted = ['{', '}'].join('\n');
+const flatJson1 = {
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
 
-    expect(makeDiff({}, {})).toEqual(expexted);
+const flatJson2 = {
+  timeout: 20,
+  verbose: true,
+  host: 'hexlet.io',
+};
+
+describe('genDiff', () => {
+  it('empty data', () => {
+    const expected = ['{', '}'].join('\n');
+
+    expect(makeDiff({}, {})).toEqual(expected);
+  });
+
+  it('flat json', () => {
+    const expected = readFile('flat.txt');
+
+    expect(makeDiff(flatJson1, flatJson2)).toEqual(expected);
   });
 });
