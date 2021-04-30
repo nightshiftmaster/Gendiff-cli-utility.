13870a930a24258a97a3eb604c860782a5ff5ef7
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2021-04-30 12:30:40 +0300

    default formatter edit

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import buildDiff from '../build-difference.js';
+import stylish from './stylish.js';
 
-const genDiff = (file1, file2, formatter) => formatter(buildDiff(file1, file2));
+const genDiff = (file1, file2, formatter = stylish) => formatter(buildDiff(file1, file2));
 
 export default genDiff;
