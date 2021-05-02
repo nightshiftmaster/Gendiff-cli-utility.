625c9e4a6b8ee936c8a97398cf88fcc3a56054e2
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2021-05-02 11:45:29 +0300

    check2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import parseByType from './parsers.js';
 import buildDiff from './build-difference.js';
-import stylish from './formatters/stylish.js';
+import selectFormat from './formatters/index.js';
 
-const genDiff = (file1, file2) => stylish(buildDiff(parseByType(file1), parseByType(file2)));
+const genDiff = (file1, file2, formatName = 'stylish') => {
+  const currFormat = selectFormat(formatName);
+  return currFormat(buildDiff(parseByType(file1), parseByType(file2)));
+};
 
 export default genDiff;
