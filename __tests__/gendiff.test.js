6ca31119505f309baddbcca5fcfee901f27a7456
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2022-08-11 22:04:34 +0300

    update

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,55 @@
+import {
+  describe, it, expect,
+} from '@jest/globals';
+import { readFileSync } from 'fs';
+import yaml from 'js-yaml';
+import path from 'path';
+import buildDiff from '../src/build-difference.js';
+import stylish from '../src/formatters/stylish.js';
+import plain from '../src/formatters/plain.js';
+import json from '../src/formatters/json.js';
+import parseByType from '../src/parsers.js';
+
+const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
+
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const json1 = JSON.parse(readFile('file1.json'));
+const json2 = JSON.parse(readFile('file2.json'));
+const yml1 = yaml.load(readFile('file1.yml'));
+const yml2 = yaml.load(readFile('file2.yml'));
+
+describe('formaters tests', () => {
+  it('parse by type', () => {
+    const expectedJson = json1;
+    const expectedYaml = yml1;
+    expect(parseByType('file1.json', readFile)).toEqual(expectedJson);
+    expect(parseByType('file1.yml', readFile)).toEqual(expectedYaml);
+  });
+
+  it('empty data', () => {
+    const expected = ['{', '}'].join('\n');
+    expect(stylish(buildDiff({}, {}))).toEqual(expected);
+  });
+
+  it('nested json', () => {
+    const expected = readFile('stylish.txt');
+    expect(stylish(buildDiff(json1, json2))).toEqual(expected);
+  });
+  it('nested yml', () => {
+    const expected = readFile('stylish.txt');
+    expect(stylish(buildDiff(yml1, yml2))).toEqual(expected);
+  });
+  it('plain json', () => {
+    const expected = readFile('plain.txt');
+    expect(plain(buildDiff(json1, json2))).toEqual(expected);
+  });
+  it('plain yml', () => {
+    const expected = readFile('plain.txt');
+    expect(plain(buildDiff(yml1, yml2))).toEqual(expected);
+  });
+  it('json output', () => {
+    const expected = JSON.stringify(buildDiff(json1, json2));
+    expect(json(buildDiff(json1, json2))).toEqual(expected);
+  });
+});
