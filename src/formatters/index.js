5fb72b1d391ba0371783948f6411cf2056d3c6f0
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2022-08-24 18:23:53 +0300

    formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,9 +2,8 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const selectFormat = (format) => {
-  const formatters = { stylish, plain, json };
-  return formatters[format];
-};
+const formatters = { stylish, plain, json };
+
+const selectFormat = (format) => formatters[format];
 
 export default selectFormat;
