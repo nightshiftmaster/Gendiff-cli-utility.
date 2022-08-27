bc3d92850917e95c386247d3be4a4df47c87832c
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2022-08-27 22:04:26 +0300

    stylish & test update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,48 +1,28 @@
 import {
-  describe, it, expect,
+  describe, expect, beforeAll, test,
 } from '@jest/globals';
 import { readFileSync } from 'fs';
-import yaml from 'js-yaml';
 import path from 'path';
-import buildDiff from '../src/build-difference.js';
-import stylish from '../src/formatters/stylish.js';
-import plain from '../src/formatters/plain.js';
-import json from '../src/formatters/json.js';
-// import { parseFile } from '../src/index.js';
+import genDiff from '../src/index.js';
 
-const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
+let stylishResult;
+let plainResult;
+let jsonResult;
 
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+beforeAll(() => {
+  stylishResult = readFileSync(path.join(process.cwd(), '__fixtures__', 'stylish.txt'), 'utf-8');
+  plainResult = readFileSync(path.join(process.cwd(), '__fixtures__', 'plain.txt'), 'utf-8');
+  jsonResult = readFileSync(path.join(process.cwd(), '__fixtures__', 'json.txt'), 'utf-8');
+});
 
-const json1 = JSON.parse(readFile('file1.json'));
-const json2 = JSON.parse(readFile('file2.json'));
-const yml1 = yaml.load(readFile('file1.yml'));
-const yml2 = yaml.load(readFile('file2.yml'));
+const extensions = ['json', 'yml'];
 
-describe('formaters tests', () => {
-  it('empty data', () => {
-    const expected = ['{', '}'].join('\n');
-    expect(stylish(buildDiff({}, {}))).toEqual(expected);
-  });
-
-  it('nested json', () => {
-    const expectedStylish = readFile('stylish.txt');
-    expect(stylish(buildDiff(json1, json2))).toEqual(expectedStylish);
-  });
-  it('nested yml', () => {
-    const expectedStylish2 = readFile('stylish.txt');
-    expect(stylish(buildDiff(yml1, yml2))).toEqual(expectedStylish2);
-  });
-  it('plain json', () => {
-    const expectedPlain = readFile('plain.txt');
-    expect(plain(buildDiff(json1, json2))).toEqual(expectedPlain);
-  });
-  it('plain yml', () => {
-    const expectedYml = readFile('plain.txt');
-    expect(plain(buildDiff(yml1, yml2))).toEqual(expectedYml);
-  });
-  it('json output', () => {
-    const expectedJson = JSON.stringify(buildDiff(json1, json2));
-    expect(json(buildDiff(json1, json2))).toEqual(expectedJson);
+describe('get difference of two files', () => {
+  test.each(extensions)('files format', (extention) => {
+    const file1 = `${process.cwd()}/__fixtures__/file1.${extention}`;
+    const file2 = `${process.cwd()}/__fixtures__/file2.${extention}`;
+    expect(genDiff(file1, file2, 'stylish')).toEqual(stylishResult);
+    expect(genDiff(file1, file2, 'plain')).toEqual(plainResult);
+    expect(genDiff(file1, file2, 'json')).toEqual(jsonResult);
   });
 });
